5fc86733976c976e8a9f7d6f93aa30d0fbc3a51d
Benedikt Spranger <b.spranger@linutronix.de>
2016-03-18 15:49:24 +0100

    Support non ssh transports
    Remove the hardcoded "+ssh" transport to be able to use qemu+tls.
    
    Signed-off-by: Benedikt Spranger <b.spranger@linutronix.de>

--- src/main/java/hudson/plugins/libvirt/Hypervisor.java
+++ src/main/java/hudson/plugins/libvirt/Hypervisor.java
@@ -432,9 +432,6 @@ public class Hypervisor extends Cloud {
                 if (hypervisorType == null) {
                     return FormValidation.error("Hypervisor type is not specified!");
                 }
-                if (username == null) {
-                    return FormValidation.error("Username is not specified!");
-                }
 
                 ConnectionBuilder builder = ConnectionBuilder.newBuilder()
                         .hypervisorType(hypervisorType)
@@ -496,13 +493,17 @@ public class Hypervisor extends Cloud {
             return username;
         }
 
-        public List<String> getHypervisorTypes() {
-            List<String> types = new ArrayList<String>();
-            types.add("QEMU");
-            types.add("XEN");
-            types.add("LXC");
-            types.add("BHYVE");
-            return types;
-        }
+	public List<String> getHypervisorTypes() {
+	    List<String> types = new ArrayList<String>();
+	    types.add("QEMU");
+	    types.add("QEMU+SSH");
+	    types.add("XEN");
+	    types.add("XEN+SSH");
+	    types.add("LXC");
+	    types.add("LXC+SSH");
+	    types.add("BHYVE");
+	    types.add("BHYVE+SSH");
+	    return types;
+	}
     }
 }
